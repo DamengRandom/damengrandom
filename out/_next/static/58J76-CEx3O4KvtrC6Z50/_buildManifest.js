a938a000551fb14dfad197f98a894cd27b1c9989
Damon Wu <damonmaozewu@gmail.com>
2020-12-01 22:22:19 +1100

    rebuild for 01/12/2020

---
+++ out/_next/static/58J76-CEx3O4KvtrC6Z50/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST = {__rewrites:[],"/":["static\u002Fchunks\u002Fpages\u002Findex-6b99ffc82d34467fcdf2.js"],"/_error":["static\u002Fchunks\u002Fpages\u002F_error-1991bc1f8e96341bd271.js"],sortedPages:["\u002F","\u002F_app","\u002F_error"]};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
